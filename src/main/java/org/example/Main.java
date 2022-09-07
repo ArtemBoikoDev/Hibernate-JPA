a6c76db0b6666f2e7ba1f0054bcf1b20a1ed5604
Artem Boiko <boiko.artem@protonmail.com>
2022-09-07 11:55:03 +0300

    Tested merge

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -9,15 +9,52 @@ public class Main {
     public static void main(String[] args) {
         EntityManagerFactory factory = Persistence.createEntityManagerFactory(
                 "MySQL-persistence-unit");
+
+        Person person = createNewPerson(factory);
+        findInSession(factory);
+//        deleteDetached(person, factory); // exception
+        deleteAttached(person, factory); // ok
+    }
+
+    /**
+     * deletes reAttached
+     */
+    private static void deleteAttached(Person person, EntityManagerFactory factory) {
         EntityManager em = factory.createEntityManager();
-
-
         em.getTransaction().begin();
 
 
-        Person person = getNewPerson();
-//        em.persist(person);
-        person = em.find(Person.class, 1L);
+        Person attachedToSessionPerson = em.merge(person);
+        em.remove(attachedToSessionPerson);
+
+
+        em.getTransaction().commit();
+        em.close();
+    }
+
+    /**
+     * doesn't delete detached ("Removing a detached instance")
+     */
+    private static void deleteDetached(Person person, EntityManagerFactory factory) {
+        EntityManager em = factory.createEntityManager();
+        em.getTransaction().begin();
+
+
+        em.remove(person);
+
+
+        em.getTransaction().commit();
+        em.close();
+    }
+
+    /**
+     * updates
+     */
+    private static void findInSession(EntityManagerFactory factory) {
+        EntityManager em = factory.createEntityManager();
+        em.getTransaction().begin();
+
+        Person person = em.find(Person.class, 1L);
         System.out.println("person = " + person);
 
         person.setFirstName("NewFirstName");
@@ -26,18 +63,24 @@ public class Main {
         System.out.println("person = " + person);
 
         em.getTransaction().commit();
-
-
-
-
-
         em.close();
     }
 
-    public static Person getNewPerson() {
+    public static Person createNewPerson(EntityManagerFactory factory) {
         Person person = new Person();
         person.setFirstName("FirstName");
         person.setLastName("LastName");
+
+
+        EntityManager em = factory.createEntityManager();
+        em.getTransaction().begin();
+
+        em.persist(person);
+        person = em.find(Person.class, 1L);
+        System.out.println("person = " + person);
+
+        em.getTransaction().commit();
+        em.close();
         return person;
     }
 }
