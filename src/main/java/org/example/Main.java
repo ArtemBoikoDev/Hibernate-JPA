43150cf17971bba5b58a22ed68b15677140e96ca
Artem Boiko <boiko.artem@protonmail.com>
2022-11-02 09:59:33 +0200

    Added logger

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -14,30 +14,61 @@ public class Main {
 
         Person person = createNewPerson(factory);
         findInSession(factory);
-//        deleteDetached(person, factory); // exception
+        deleteDetached(person, factory); // exception
         deleteAttached(person, factory); // ok
 
 
         factory.close();
     }
 
-    /**
-     * deletes reAttached
-     */
-    private static void deleteAttached(Person person, EntityManagerFactory factory) {
+    public static Person createNewPerson(EntityManagerFactory factory) {
+        Person person = new Person();
+        person.setFirstName("FirstName");
+        person.setLastName("LastName");
+
+
         EntityManager em = factory.createEntityManager();
         try {
             em.getTransaction().begin();
 
 
-            Person attachedToSessionPerson = em.merge(person);
-            em.remove(attachedToSessionPerson);
+            em.persist(person);
+            person = em.find(Person.class, 1L);
+            log.info("person = {}", person);
 
 
             em.getTransaction().commit();
         } catch (Exception exception) {
             em.getTransaction().rollback();
-            log.error("Cannot execute method 'deleteAttached(Person person, EntityManagerFactory factory)'");
+            log.error("Cannot execute method 'findInSession(EntityManagerFactory factory)'");
+        } finally {
+            em.close();
+        }
+        return person;
+    }
+
+    /**
+     * updates
+     */
+    private static void findInSession(EntityManagerFactory factory) {
+        EntityManager em = factory.createEntityManager();
+        try {
+            em.getTransaction().begin();
+
+
+            Person person = em.find(Person.class, 1L);
+            log.info("person before update = {}", person);
+
+            person.setFirstName("NewFirstName");
+
+            person = em.find(Person.class, 1L);
+            log.info("person  after update = {}", person);
+
+
+            em.getTransaction().commit();
+        } catch (Exception exception) {
+            em.getTransaction().rollback();
+            log.error("Cannot execute method 'findInSession(EntityManagerFactory factory)'");
         } finally {
             em.close();
         }
@@ -65,55 +96,25 @@ public class Main {
     }
 
     /**
-     * updates
+     * deletes reAttached
      */
-    private static void findInSession(EntityManagerFactory factory) {
+    private static void deleteAttached(Person person, EntityManagerFactory factory) {
         EntityManager em = factory.createEntityManager();
         try {
             em.getTransaction().begin();
 
 
-            Person person = em.find(Person.class, 1L);
-            log.info("person = {}", person);
-
-            person.setFirstName("NewFirstName");
-
-            person = em.find(Person.class, 1L);
-            log.info("person = {}", person);
+            Person attachedToSessionPerson = em.merge(person);
+            em.remove(attachedToSessionPerson);
 
 
             em.getTransaction().commit();
         } catch (Exception exception) {
             em.getTransaction().rollback();
-            log.error("Cannot execute method 'findInSession(EntityManagerFactory factory)'");
+            log.error("Cannot execute method 'deleteAttached(Person person, EntityManagerFactory factory)'");
         } finally {
             em.close();
         }
-    }
-
-    public static Person createNewPerson(EntityManagerFactory factory) {
-        Person person = new Person();
-        person.setFirstName("FirstName");
-        person.setLastName("LastName");
-
-
-        EntityManager em = factory.createEntityManager();
-        try {
-            em.getTransaction().begin();
-
-
-            em.persist(person);
-            person = em.find(Person.class, 1L);
-            log.info("person = {}", person);
-
-
-            em.getTransaction().commit();
-        } catch (Exception exception) {
-            em.getTransaction().rollback();
-            log.error("Cannot execute method 'findInSession(EntityManagerFactory factory)'");
-        } finally {
-            em.close();
-        }
-        return person;
+        log.info("person has been deleted");
     }
 }
